41a2e6f7516e6655e2f534864419aebb9bc3d06e
اُسامہ وحید <85633117+ToucH9000@users.noreply.github.com>
2024-02-07 10:34:12 +0500

    Add files via upload

--- Module/service.sh
+++ Module/service.sh
@@ -1,66 +1,56 @@
-# Sensitive properties
+# Conditional sensitive properties
 
-RESETPROP="${0%/*}/resetprop"
-
-if [ -e "$RESETPROP" ]; then
-	chmod 755 $RESETPROP
-else
-	RESETPROP="resetprop"
-fi
-
-check_resetprop() {
-  local NAME=$1
-  local EXPECTED=$2
-  local VALUE=$(resetprop $NAME)
-  [ -z $VALUE ] || [ $VALUE = $EXPECTED ] || $RESETPROP -n $NAME $EXPECTED
+resetprop_if_diff() {
+    local NAME=$1
+    local EXPECTED=$2
+    local CURRENT=$(resetprop $NAME)
+    
+    [ -z "$CURRENT" ] || [ "$CURRENT" == "$EXPECTED" ] || resetprop $NAME $EXPECTED
 }
 
-maybe_set_prop() {
-    local prop="$1"
-    local contains="$2"
-    local value="$3"
-
-    if [[ "$(getprop "$prop")" == *"$contains"* ]]; then
-        $RESETPROP -n "$prop" "$value"
-    fi
+resetprop_if_match() {
+    local NAME=$1
+    local CONTAINS=$2
+    local VALUE=$3
+    
+    [[ "$(resetprop $NAME)" == *"$CONTAINS"* ]] && resetprop $NAME $VALUE
 }
 
 # Magisk recovery mode
-maybe_set_prop ro.bootmode recovery unknown
-maybe_set_prop ro.boot.mode recovery unknown
-maybe_set_prop vendor.boot.mode recovery unknown
+resetprop_if_match ro.bootmode recovery unknown
+resetprop_if_match ro.boot.mode recovery unknown
+resetprop_if_match vendor.boot.mode recovery unknown
 
-# Hiding SELinux | Permissive status
-if [ -n "$(getprop ro.build.selinux)" ]; then
-	$RESETPROP --delete ro.build.selinux
+# SELinux
+if [ -n "$(resetprop ro.build.selinux)" ]; then
+    resetprop --delete ro.build.selinux
 fi
 
-# Hiding SELinux | Use toybox to protect *stat* access time reading
+# use toybox to protect *stat* access time reading
 if [ "$(toybox cat /sys/fs/selinux/enforce)" == "0" ]; then
     chmod 640 /sys/fs/selinux/enforce
     chmod 440 /sys/fs/selinux/policy
 fi
 
-# Reset props after boot completed to avoid breaking some weird devices/ROMs...
+# SafetyNet/Play Integrity
 {
-    until [[ "$(resetprop sys.boot_completed)" == "1" ]]; do
+    # must be set after boot_completed for various OEMs
+    until [ "$(getprop sys.boot_completed)" == "1" ]; do
         sleep 1
     done
 
-    # Prop from HuskyDG
-    check_resetprop sys.oem_unlock_allowed 0
+    # avoid breaking Realme fingerprint scanners
+    resetprop_if_diff ro.boot.flash.locked 1
+    resetprop_if_diff ro.boot.realme.lockstate 1
 
-    # SafetyNet/Play Integrity | Avoid breaking Realme fingerprint scanners
-    check_resetprop ro.boot.flash.locked 1
+    # avoid breaking Oppo fingerprint scanners
+    resetprop_if_diff ro.boot.vbmeta.device_state locked
 
-    # SafetyNet/Play Integrity | Avoid breaking Oppo fingerprint scanners
-    check_resetprop ro.boot.vbmeta.device_state locked
+    # avoid breaking OnePlus display modes/fingerprint scanners
+    resetprop_if_diff vendor.boot.verifiedbootstate green
 
-    # SafetyNet/Play Integrity | Avoid breaking OnePlus display modes/fingerprint scanners
-    check_resetprop vendor.boot.verifiedbootstate green
-
-    # SafetyNet/Play Integrity | Avoid breaking OnePlus display modes/fingerprint scanners on OOS 12
-    check_resetprop ro.boot.verifiedbootstate green
-    check_resetprop ro.boot.veritymode enforcing
-    check_resetprop vendor.boot.vbmeta.device_state locked
+    # avoid breaking OnePlus/Oppo display fingerprint scanners on OOS/ColorOS 12+
+    resetprop_if_diff ro.boot.verifiedbootstate green
+    resetprop_if_diff ro.boot.veritymode enforcing
+    resetprop_if_diff vendor.boot.vbmeta.device_state locked
 }&
