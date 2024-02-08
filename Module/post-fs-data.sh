ef81b7d97a8d027b1d0b2f90f638481800b9ea4d
اُسامہ وحید <85633117+ToucH9000@users.noreply.github.com>
2024-02-08 06:50:10 +0500

    Add files via upload

--- Module/post-fs-data.sh
+++ Module/post-fs-data.sh
@@ -24,12 +24,16 @@ resetprop_if_match() {
 # RootBeer, Microsoft
 resetprop_if_diff ro.build.tags release-keys
 resetprop_if_diff ro.build.type user
+resetprop_if_diff ro.product.name vayu
 
 # Other
 resetprop_if_diff ro.debuggable 0
 resetprop_if_diff ro.secure 1
 for prefix in bootimage odm product system system_ext vendor vendor_dlkm; do
     check_resetprop ro.${prefix}.build.type user
+done
+for prefix in bootimage odm product system system_ext vendor vendor_dlkm; do
+    check_resetprop ro.product.${prefix}.name vayu
 done
 
 # Lineage
